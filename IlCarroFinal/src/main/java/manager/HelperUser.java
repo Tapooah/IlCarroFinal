bfe5c7f9350254d185c96f1e261d43ae52d59a03
Tapooah <bilyk.po@gmail.com>
2023-10-30 13:50:48 +0200

    101

--- IlCarroFinal/src/main/java/manager/HelperUser.java
+++ IlCarroFinal/src/main/java/manager/HelperUser.java
@@ -2,8 +2,10 @@ package manager;
 
 import madels.User;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
 
@@ -48,8 +50,24 @@ public class HelperUser extends HelperBase {
     }
 
     public void checkPolicy() {
+//        click(By.id("terms-of-use"));
+        click(By.cssSelector("label[for='terms-of-use']"));
+    }
+
+    public void checkPolicyXY() {
+        WebElement label = wd.findElement(By.cssSelector("label[for='terms-of-use']"));
+        Rectangle rect = label.getRect();
+        int xOffSet = rect.getWidth() / 2;
+        int yOffSet = rect.getHeight() / 2;
+
+        Actions actions = new Actions(wd);
+        actions.moveToElement(label).release().perform();
+        actions.moveByOffset(-xOffSet, -yOffSet).click().release().perform();
 
     }
 
+    public void clickOK() {
+        click(By.xpath("//button[text()='Ok']"));
+    }
     ////////////////////////////////////// ˄ service methods ˄ //////////////////////////////////////
 }
--- IlCarroFinal/src/test/java/tests/RegistrationTests.java
+++ IlCarroFinal/src/test/java/tests/RegistrationTests.java
@@ -1,18 +1,31 @@
 package tests;
 
 import madels.User;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class RegistrationTests extends TestBase {
     @Test
-    public void registrationSuccess() {
+    public void registrationSuccess1() {
         int i = (int) (System.currentTimeMillis() / 1000)%3600;
         User user = new User().setName("Liza").setLastName("Snow").setEmail("redfoxy" + i + "@gmail.com").setPassword("Ff12345$");
 
         app.getHelperUser().openRegistrationForm();
         app.getHelperUser().fillRegistrationForm(user);
-        app.getHelperUser().checkPolicy();
+        app.getHelperUser().checkPolicyXY();
         app.getHelperUser().submit();
+        Assert.assertEquals(app.getMessage(), "Registered");
+        app.getHelperUser().clickOK();
     }
 
+//    @Test //doesn't work with these Terms of Use
+//    public void registrationSuccess2() {
+//        int i = (int) (System.currentTimeMillis() / 1000)%3600;
+//        User user = new User().setName("Liza").setLastName("Snow").setEmail("redfoxy" + i + "@gmail.com").setPassword("Ff12345$");
+//
+//        app.getHelperUser().openRegistrationForm();
+//        app.getHelperUser().fillRegistrationForm(user);
+//        app.getHelperUser().checkPolicy();
+//        app.getHelperUser().submit();
+//    }
 }
